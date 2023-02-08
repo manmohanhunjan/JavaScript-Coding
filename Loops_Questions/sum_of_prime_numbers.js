1e2049e00be30f5d8e36b6b5bd680db26c924c09
Manmohan Singh Hunjan <71703296+manmohanhunjan@users.noreply.github.com>
2023-02-08 14:50:18 +0530

    commit 2

--- Loops_Questions/sum_of_prime_numbers.js
+++ Loops_Questions/sum_of_prime_numbers.js
@@ -1,1 +1,23 @@
 let n = +prompt("Enter a number");
+let i = 1;
+let sum = 0;
+while (i<=n) {
+    let tracker = 0;
+    let j = 2;
+    while (j<i) {
+        if (i%j==0) {
+            console.log(i + "is Not a Prime");
+            break;
+        } else{
+
+            console.log(i + "is a Prime");
+            tracker += i
+        }
+        j++;
+    }
+    if (tracker>0) {
+        sum += i;
+    }
+    i++
+}
+console.log("sum is" + sum);
