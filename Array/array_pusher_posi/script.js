0592c29863e11d7e3edad72d6eb3c1cab14c7982
Manmohan Singh Hunjan <71703296+manmohanhunjan@users.noreply.github.com>
2023-04-07 18:25:55 +0530

    ....

---
+++ Array/array_pusher_posi/script.js
@@ -0,0 +1,11 @@
+function abc() {
+    const temp = [];
+    let pos = document.getElementById("pos").value;
+    let no = document.getElementById("no").value;
+    let dta1 = +document.getElementById("dta1").value;
+    let dta2 = +document.getElementById("dta2").value;
+    let dta3 = +document.getElementById("dta3").value;
+    let dta4 = +document.getElementById("dta4").value;
+    let dta5 = +document.getElementById("dta5").value;
+    let dta6 = +document.getElementById("dta6").value;
+}
