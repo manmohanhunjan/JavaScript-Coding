6579258a693ad92ac148a00fb89cdb070ded6b7a
Manmohan Hunjan <71703296+manmohanhunjan@users.noreply.github.com>
2023-09-28 05:47:55 +0000

    comparison.js

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+//! Avoid below comparisons
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+
+//! ----------------------------
+
+// === strict equality
+
+console.log("2" === 2);
