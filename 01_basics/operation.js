c556f08a9b1bb013b5562e6608e4e5126e62d5b2
Manmohan Hunjan <71703296+manmohanhunjan@users.noreply.github.com>
2023-09-28 05:40:16 +0000

    operation.js

---
+++ 01_basics/operation.js
@@ -0,0 +1,33 @@
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+// console.log(2 ** 2);
+
+// let str1 = "Hello";
+// let str2 = " World";
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(1 + 2 + "3");
+
+// console.log((3 + 2) * 2 % 3) ;
+
+// console.log(+true); // do not use
+// console.log(+""); // do not use
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 3 + 2;
+
+let gameCounter = 100;
+
+++gameCounter;
+
+console.log(gameCounter);
