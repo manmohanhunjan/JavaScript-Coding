15ba14ab486fad261c0020739f0113d50888058e
Manmohan Singh Hunjan <71703296+manmohanhunjan@users.noreply.github.com>
2023-09-30 13:32:19 +0530

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,50 @@
+// if
+
+// const isUserloggedIn = true;
+// const temperature = 41;
+
+// if (temperature === 40) {
+//   console.log("less than 50");
+// } else {
+//   console.log("temperature is greater than 50");
+// }
+//? >, <, <=, >=, ==, ===, !=, !==
+
+// const score = 200;
+
+// if (score > 100) {
+//     var power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+//! implicit scope
+
+const balance = 1000;
+
+// if (balance > 500) console.log("test");
+// if (balance > 500) console.log("test"), console.log("test2"); //* bad practice don't do this
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// }else if (balance < 750) {
+//     console.log("less than 750");
+// }else if (balance < 900) {
+//     console.log("less than 750");
+// }else{
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2 == 2) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromEmail || loggedInFromGoogle) {
+    console.log("User logged in");
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "January";
+
+switch (month) {
+    case "January":
+        console.log("January");
+        break;
+    case "February":
+        console.log("February");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("March");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
